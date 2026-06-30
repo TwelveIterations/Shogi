7c0528addb158141072831510f93264162d00af4
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-06-30 20:50:10 +0200

    feat: Add support for RefusalInformation -> Component coercion

--- common/src/main/java/net/blay09/mods/shogi/common/effect/failure/RefusalInformation.java
+++ common/src/main/java/net/blay09/mods/shogi/common/effect/failure/RefusalInformation.java
@@ -1,15 +1,21 @@
 package net.blay09.mods.shogi.common.effect.failure;
 
+import net.blay09.mods.shogi.coercion.ComponentHolder;
 import net.blay09.mods.shogi.effect.failure.ShogiFatalFailure;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.ComponentSerialization;
 import net.minecraft.network.codec.StreamCodec;
 
-public record RefusalInformation(Component message) implements ShogiFatalFailure {
+public record RefusalInformation(Component message) implements ShogiFatalFailure, ComponentHolder {
     public static final StreamCodec<RegistryFriendlyByteBuf, RefusalInformation> STREAM_CODEC = StreamCodec.composite(
             ComponentSerialization.STREAM_CODEC,
             RefusalInformation::message,
             RefusalInformation::new
     );
+
+    @Override
+    public Component component() {
+        return message;
+    }
 }
