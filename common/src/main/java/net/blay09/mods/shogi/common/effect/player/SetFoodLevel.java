72761dfad9024d9402a14b78f7b9aa4887d74ccd
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-07-01 13:53:36 +0200

    feat: Add set_food_level and set_health effects

---
+++ common/src/main/java/net/blay09/mods/shogi/common/effect/player/SetFoodLevel.java
@@ -0,0 +1,36 @@
+package net.blay09.mods.shogi.common.effect.player;
+
+import com.mojang.datafixers.util.Either;
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.blay09.mods.shogi.coercion.Coercion;
+import net.blay09.mods.shogi.context.ShogiContext;
+import net.blay09.mods.shogi.effect.EffectArgumentCodecs;
+import net.blay09.mods.shogi.effect.ShogiEffect;
+import net.blay09.mods.shogi.scope.ShogiScope;
+import net.minecraft.resources.Identifier;
+
+public record SetFoodLevel(ShogiEffect<?> foodLevel) implements ShogiEffect<Integer> {
+
+    public static final Identifier IDENTIFIER = Identifier.fromNamespaceAndPath("shogi", "set_food_level");
+
+    public static MapCodec<SetFoodLevel> mapCodec(ShogiScope scope) {
+        return RecordCodecBuilder.mapCodec(instance -> instance.group(
+                EffectArgumentCodecs.effectOrConstant(scope).fieldOf("food_level").forGetter(SetFoodLevel::foodLevel)
+        ).apply(instance, SetFoodLevel::new));
+    }
+
+    @Override
+    public Either<Integer, Object> apply(ShogiContext context) {
+        final var player = context.requirePlayer();
+        final int foodLevelAmount = foodLevel.apply(context).mapLeft(Coercion.NON_NEGATIVE_INT).orThrow();
+        final int previousFoodLevel = player.getFoodData().getFoodLevel();
+        context.execute(IDENTIFIER, () -> player.getFoodData().setFoodLevel(foodLevelAmount));
+        return Either.left(previousFoodLevel);
+    }
+
+    @Override
+    public Identifier identifier() {
+        return IDENTIFIER;
+    }
+}
