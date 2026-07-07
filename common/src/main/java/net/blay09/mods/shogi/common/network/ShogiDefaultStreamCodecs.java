78d02741c14d67cf7e9508c50282443ff5ed53d3
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-07-07 21:52:52 +0200

    fix: Added missing sync codecs

--- common/src/main/java/net/blay09/mods/shogi/common/network/ShogiDefaultStreamCodecs.java
+++ common/src/main/java/net/blay09/mods/shogi/common/network/ShogiDefaultStreamCodecs.java
@@ -1,12 +1,26 @@
 package net.blay09.mods.shogi.common.network;
 
 import com.google.gson.JsonElement;
+import net.blay09.mods.shogi.common.effect.compose.AggregateEffect;
+import net.blay09.mods.shogi.common.effect.compose.AndEffect;
+import net.blay09.mods.shogi.common.effect.compose.AnyEffect;
+import net.blay09.mods.shogi.common.effect.compose.ConditionEffect;
+import net.blay09.mods.shogi.common.effect.compose.NotEffect;
 import net.blay09.mods.shogi.common.effect.cost.*;
 import net.blay09.mods.shogi.common.effect.failure.FailureInformation;
 import net.blay09.mods.shogi.common.effect.failure.RefusalInformation;
 import net.blay09.mods.shogi.common.effect.server.cooldown.CooldownInformation;
+import net.blay09.mods.shogi.common.effect.variable.AssignmentEffect;
+import net.blay09.mods.shogi.common.effect.variable.BinaryOpEffect;
+import net.blay09.mods.shogi.common.effect.variable.ClampEffect;
+import net.blay09.mods.shogi.common.effect.variable.ClampMaxEffect;
+import net.blay09.mods.shogi.common.effect.variable.ClampMinEffect;
+import net.blay09.mods.shogi.common.effect.variable.MacroAssignmentEffect;
 import net.blay09.mods.shogi.common.effect.variable.MissingVariableFailure;
+import net.blay09.mods.shogi.common.effect.variable.VariableEffect;
+import net.blay09.mods.shogi.effect.ConstantEffect;
 import net.blay09.mods.shogi.effect.EmptyEffect;
+import net.blay09.mods.shogi.effect.ShogiEffect;
 import net.blay09.mods.shogi.effect.ShogiEmpty;
 import net.blay09.mods.shogi.effect.failure.ShogiDeferred;
 import net.blay09.mods.shogi.network.ShogiStreamCodecs;
@@ -25,6 +39,10 @@ public final class ShogiDefaultStreamCodecs {
     private static final StreamCodec<RegistryFriendlyByteBuf, Throwable> THROWABLE_CODEC = StreamCodec.unit(new Exception());
     private static final StreamCodec<RegistryFriendlyByteBuf, ShogiDeferred> DEFERRED_CODEC = StreamCodec.unit(ShogiDeferred.INSTANCE);
     private static final StreamCodec<RegistryFriendlyByteBuf, ShogiEmpty> EMPTY_CODEC = StreamCodec.unit(EmptyEffect.INSTANCE);
+    private static final StreamCodec<RegistryFriendlyByteBuf, JsonElement> JSON_CODEC = ByteBufCodecs.lenientJson(Short.MAX_VALUE).cast();
+    private static final StreamCodec<RegistryFriendlyByteBuf, ShogiEffect<?>> EFFECT_CODEC = ShogiStreamCodecs.dynamicObjectCodec()
+            .map(value -> (ShogiEffect<?>) value, value -> value);
+    private static final StreamCodec<RegistryFriendlyByteBuf, List<ShogiEffect<?>>> EFFECT_LIST_CODEC = EFFECT_CODEC.apply(ByteBufCodecs.list());
 
     private ShogiDefaultStreamCodecs() {
     }
@@ -39,7 +57,7 @@ public final class ShogiDefaultStreamCodecs {
         ShogiStreamCodecs.register(id("float"), Float.class, ByteBufCodecs.FLOAT.cast());
         ShogiStreamCodecs.register(id("bool"), Boolean.class, ByteBufCodecs.BOOL.cast());
         ShogiStreamCodecs.register(id("string"), String.class, ByteBufCodecs.STRING_UTF8.cast());
-        ShogiStreamCodecs.register(id("json"), JsonElement.class, ByteBufCodecs.lenientJson(Short.MAX_VALUE).cast());
+        ShogiStreamCodecs.register(id("json"), JsonElement.class, JSON_CODEC.cast());
         ShogiStreamCodecs.register(id("component"), Component.class, ComponentSerialization.STREAM_CODEC);
         ShogiStreamCodecs.register(id("throwable"), Throwable.class, THROWABLE_CODEC);
         ShogiStreamCodecs.register(id("list"), List.class, ShogiStreamCodecs.LIST_STREAM_CODEC);
@@ -54,5 +72,90 @@ public final class ShogiDefaultStreamCodecs {
         ShogiStreamCodecs.register(id("cooldown"), CooldownInformation.class, CooldownInformation.STREAM_CODEC);
         ShogiStreamCodecs.register(id("deferred"), ShogiDeferred.class, DEFERRED_CODEC);
         ShogiStreamCodecs.register(id("empty"), ShogiEmpty.class, EMPTY_CODEC);
+        ShogiStreamCodecs.register(id("constant_effect"), ConstantEffect.class, StreamCodec.composite(
+                JSON_CODEC,
+                ConstantEffect::value,
+                ConstantEffect::new
+        ));
+        ShogiStreamCodecs.register(id("aggregate_effect"), AggregateEffect.class, StreamCodec.composite(
+                EFFECT_LIST_CODEC,
+                AggregateEffect::effects,
+                AggregateEffect::new
+        ));
+        ShogiStreamCodecs.register(id("and_effect"), AndEffect.class, StreamCodec.composite(
+                EFFECT_LIST_CODEC,
+                AndEffect::conditions,
+                AndEffect::new
+        ));
+        ShogiStreamCodecs.register(id("any_effect"), AnyEffect.class, StreamCodec.composite(
+                EFFECT_LIST_CODEC,
+                AnyEffect::conditions,
+                AnyEffect::new
+        ));
+        ShogiStreamCodecs.register(id("not_effect"), NotEffect.class, StreamCodec.composite(
+                EFFECT_CODEC,
+                NotEffect::condition,
+                NotEffect::new
+        ));
+        ShogiStreamCodecs.register(id("condition_effect"), ConditionEffect.class, StreamCodec.composite(
+                EFFECT_CODEC,
+                ConditionEffect::condition,
+                EFFECT_CODEC,
+                ConditionEffect::trueEffect,
+                EFFECT_CODEC,
+                ConditionEffect::falseEffect,
+                ConditionEffect::new
+        ));
+        ShogiStreamCodecs.register(id("assignment_effect"), AssignmentEffect.class, StreamCodec.composite(
+                ByteBufCodecs.STRING_UTF8,
+                AssignmentEffect::variable,
+                EFFECT_CODEC,
+                AssignmentEffect::value,
+                AssignmentEffect::new
+        ));
+        ShogiStreamCodecs.register(id("macro_assignment_effect"), MacroAssignmentEffect.class, StreamCodec.composite(
+                ByteBufCodecs.STRING_UTF8,
+                MacroAssignmentEffect::variable,
+                EFFECT_CODEC,
+                MacroAssignmentEffect::value,
+                MacroAssignmentEffect::new
+        ));
+        ShogiStreamCodecs.register(id("variable_effect"), VariableEffect.class, StreamCodec.composite(
+                ByteBufCodecs.STRING_UTF8,
+                VariableEffect::name,
+                VariableEffect::new
+        ));
+        ShogiStreamCodecs.register(id("binary_op_effect"), BinaryOpEffect.class, StreamCodec.composite(
+                ByteBufCodecs.STRING_UTF8,
+                BinaryOpEffect::op,
+                EFFECT_CODEC,
+                BinaryOpEffect::left,
+                EFFECT_CODEC,
+                BinaryOpEffect::right,
+                BinaryOpEffect::new
+        ));
+        ShogiStreamCodecs.register(id("clamp_min_effect"), ClampMinEffect.class, StreamCodec.composite(
+                EFFECT_CODEC,
+                ClampMinEffect::value,
+                EFFECT_CODEC,
+                ClampMinEffect::min,
+                ClampMinEffect::new
+        ));
+        ShogiStreamCodecs.register(id("clamp_max_effect"), ClampMaxEffect.class, StreamCodec.composite(
+                EFFECT_CODEC,
+                ClampMaxEffect::value,
+                EFFECT_CODEC,
+                ClampMaxEffect::max,
+                ClampMaxEffect::new
+        ));
+        ShogiStreamCodecs.register(id("clamp_effect"), ClampEffect.class, StreamCodec.composite(
+                EFFECT_CODEC,
+                ClampEffect::value,
+                EFFECT_CODEC,
+                ClampEffect::min,
+                EFFECT_CODEC,
+                ClampEffect::max,
+                ClampEffect::new
+        ));
     }
 }
