cfc532419602562ddc4db47fd11ff2bb1cd81ba5
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-09-26 08:09:23 +0200

    fix: Fix circular reference resulting in crash with mods including shogi-api without a shogi dependency #6

---
+++ common/src/main/java/net/blay09/mods/shogi/common/ShogiBuiltinRules.java
@@ -0,0 +1,16 @@
+package net.blay09.mods.shogi.common;
+
+import com.mojang.datafixers.util.Either;
+import net.blay09.mods.shogi.Shogi;
+import net.blay09.mods.shogi.ShogiValue;
+import net.blay09.mods.shogi.effect.EmptyEffect;
+import net.minecraft.world.entity.LivingEntity;
+
+public class ShogiBuiltinRules {
+
+    public static final ShogiValue<LivingEntity, ?> ON_DEATH = Shogi.defaultScope().maybe(
+            ShogiCommon.id("on_death"),
+            _ -> Either.right(EmptyEffect.INSTANCE)
+    );
+
+}
